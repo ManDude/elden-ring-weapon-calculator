f59e36fb1a3dd311e23bdc102e7a4638fe75990f
ManDude <7569514+ManDude@users.noreply.github.com>
2025-07-25 21:10:41 +0100

    derp 2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
+  base: '/elden-ring-weapon-calculator/',
   plugins: [react(), eslint()],
   build: {
     outDir: "build",
